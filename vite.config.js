e3e052b873afe4c21aeaa9a0d1669f01a6084599
Pankaj Kumar <kuldeepydv15@gmail.com>
2024-10-16 16:00:38 +0530

    port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all addresses
+    port: 3000, // Default port for local testing
+  },
 })
